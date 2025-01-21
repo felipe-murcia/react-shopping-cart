68eeebefeb706172e8b7939696db77fc24dadf29
Juan Sebastian Murcia <smurcia0820@gmail.com>
2025-01-21 12:12:22 -0500

    ajuste categorias scroll

--- src/components/Collections/Collections.tsx
+++ src/components/Collections/Collections.tsx
@@ -1,38 +1,46 @@
 import React from 'react';
-import CardProduct from '../CardProduct/CardProduct';
 import useCollections from '../../hooks/useCollections';
 
 interface CollectionsProps {
-    handleCollection: (value:any) => void;
+    handleCollection: (value: any) => void;
 }
-
 
 export const Collections = ({ handleCollection }: CollectionsProps) => {
-
     let { collections } = useCollections();
 
-  return ( 
-    <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center'}}>
-
-        {
-            collections.map((collection:any,i:number) => {
-                // console.log('collection-',collection)
-                let node = collection.node
-                return(
-                <div className="relative w-40 h-40 m-5" onClick={()=>handleCollection(node.id)} key={i}>
-                    <img 
-                        src={node.image.url}
-                        alt={node.title}
-                        className="w-full h-full object-cover rounded-lg"
-                    />
-                    <div className="absolute inset-0 bg-black bg-opacity-50 hover:bg-opacity-80  flex flex-col justify-center items-center rounded-lg">
-                        <h2 className="text-white text-xl font-bold">{node.title}</h2>
+    return (
+        <div
+            className="flex gap-5 p-5 overflow-x-auto no-scrollbar"
+            style={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+                overflowX: 'auto',
+                whiteSpace: 'nowrap',
+            }}
+        >
+            {collections.map((collection: any, i: number) => {
+                let node = collection.node;
+                return (
+                    <div
+                        className="relative w-40 h-40 flex-shrink-0"
+                        onClick={() => handleCollection(node.id)}
+                        key={i}
+                        style={{
+                            margin: '0.3rem', // Espaciado uniforme entre tarjetas
+                        }}
+                    >
+                        <img
+                            src={node.image.url}
+                            alt={node.title}
+                            className="w-full h-full object-cover rounded-lg"
+                        />
+                        <div className="absolute inset-0 bg-black bg-opacity-50 hover:bg-opacity-80 flex flex-col justify-center items-center rounded-lg">
+                            <h2 className="text-white text-xl font-bold">{node.title}</h2>
+                        </div>
                     </div>
-                </div>
-                )
-            })
-        }
-
-    </div>
-  )
-}
+                );
+            })}
+        </div>
+    );
+};
